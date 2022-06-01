34c8c08c1c9fe4165e2ffc7c4429c9f3e41e53a1
nagatani-ay <nagatani@ay-sys.com>
2022-06-01 17:29:59 +0900

    1 file modified

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -12,21 +12,27 @@ const currentMonth = ref(new Date().getMonth() + 1);
 
 const calendarArray = computed(() => {
   let calendarArray = [];
-  const firstDayWeek = new Date(currentYear.value, currentMonth.value, 1);
-  const lastDayWeek = new Date(currentYear.value, currentMonth.value, 0);
+  const firstDayWeek = new Date().getDay();
+  const lastDayWeek = new Date(
+    currentYear.value,
+    currentMonth.value,
+    0
+  ).getDate();
   const lastMonthDay = new Date(currentYear.value, currentMonth.value - 1, 0);
 
-  for (let i = 0; i < firstDayWeek.getDay(); i++) {
+  for (let i = 0; i < firstDayWeek; i++) {
     calendarArray.push({
       date: {
         year: lastMonthDay.getFullYear(),
         month: lastMonthDay.getMonth() + 1,
-        day: lastMonthDay.getDate() - i,
+        day: lastMonthDay.getDate() - firstDayWeek + i + 1,
       },
-      dayofweek: lastMonthDay.getDay(),
+      dayofweek: i,
     });
   }
+
   console.log(lastMonthDay);
+  return calendarArray;
 });
 </script>
 
