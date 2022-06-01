01cf09ea134f37448211359f3fe69923ddb0f2a5
nagatani-ay <nagatani@ay-sys.com>
2022-06-01 18:07:44 +0900

    1 file modified

--- src/components/Schedule/Calendar.vue
+++ src/components/Schedule/Calendar.vue
@@ -2,10 +2,16 @@
 import { Date, Calendar } from '../../types';
 const props = defineProps<{ calendarArray: Calendar }>();
 const emit = defineEmits<{}>();
+const dayOfWeeksJP = ['日', '月', '火', '水', '木', '金', '土'];
 </script>
 
 <template>
-  calendar
+  <div class="calendar__header">
+    <div class="calendar__dayofweek" v-for="dayofweek in dayOfWeeksJP">
+      {{ dayofweek }}
+    </div>
+  </div>
+
   <div class="calendar__table">
     <div v-for="calendar in calendarArray">
       {{ calendar.date.day }}
@@ -61,6 +67,12 @@ const emit = defineEmits<{}>();
 </template>
 
 <style>
+.calendar__header {
+  display: grid;
+  align-items: start;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-auto-rows: minmax(50px, auto);
+}
 .calendar__table {
   display: grid;
   align-items: start;
