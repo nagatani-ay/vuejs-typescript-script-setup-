1c8e0f7320b735ec4ec70a0d7cc7f3b948e0eb7f
nagatani-ay <nagatani@ay-sys.com>
2022-06-01 16:25:09 +0900

    1 file modified

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -4,33 +4,39 @@ import ScheduleMenu from '../components/Schedule/ScheduleMenu.vue';
 import TodoCalendar from '../components/Schedule/Calendar.vue';
 import { Todo } from '../types';
 
-
-const props = defineProps<{todos:Todo[]}>();
+const props = defineProps<{ todos: Todo[] }>();
 const emit = defineEmits<{}>();
 
-const currentYear=ref(new Date().getFullYear())
-const currentMonth=ref(new Date().getMonth() + 1)
+const currentYear = ref(new Date().getFullYear());
+const currentMonth = ref(new Date().getMonth() + 1);
 
+const calendarArray = computed(() => {
+  let calendarArray = [];
+  const firstDayWeek = new Date(currentYear.value, currentMonth.value, 1);
+  const lastDayWeek = new Date(currentYear.value, currentMonth.value, 0);
+  const lastMonthDay = new Date(currentYear.value,currentMonth.value - 1,0);
 
-//  const calendarArray = computed(() => {
-//    let calendarArray=[];
-//   const firstDayWeek = new Date(currentYear.value, currentMonth.value,1).getDay();
-//   const lastDayWeek = new Date(currentYear.value, currentMonth.value,0).getDay();
-// console.log(firstDayWeek)
-//   for (i=0;i<firstDayWeek ;i++){
-//     console.log(i)
-//   }
-
-
-//  });
+  for (let i = 0; i < firstDayWeek.getDay(); i++) {
+    calendarArray.push({
+      date: {
+              year: lastMonthDay.getFullYear()
+              month: lastMonthDay.getMonth() + 1,
+              day: lastMonthDay.getDate() - i,
+            },
+    dayofweek: lastMonthDay.getDay()
+    })
+};
+  console.log(lastMonthDay);
+});
 </script>
 
 <template>
-  <schedule-menu :currentMonth="currentMonth"
-  :currentYear="currentYear"></schedule-menu>
+  <schedule-menu
+    :currentMonth="currentMonth"
+    :currentYear="currentYear"
+  ></schedule-menu>
   <todo-calendar></todo-calendar>
-<!-- {{calendarArray}} -->
-  
+  {{ calendarArray }}
 </template>
 
 <style></style>
