9fa1d910c52e506042be37c6fa66646a9eb0f267
nagatani-ay <nagatani@ay-sys.com>
2022-06-07 15:07:24 +0900

    2 files modified

--- src/Function/__tests__/useTodos.test.ts
+++ src/Function/__tests__/useTodos.test.ts
@@ -1,10 +1,10 @@
-import {Todo} from "../../types"
+import { Todo } from '../../types';
 import { useTodos, getTodos } from '../useTodos';
-import { test, expect, vi, it, describe, beforeEach } from 'vitest';
+import { expect, vi, it, describe, beforeEach, afterEach } from 'vitest';
 const { add, edit, checked, remove, editData, calendarItems, createNewData } =
   useTodos();
 const todolist = getTodos();
-const testData:Todo = {
+const testData1: Todo = {
   code: 'testData1',
   text: 'test1',
   status: false,
@@ -15,7 +15,7 @@ const testData:Todo = {
     day: 22,
   },
 };
-const testData2 = {
+const testData2: Todo = {
   code: 'testData2',
   text: 'test2',
   status: false,
@@ -26,23 +26,33 @@ const testData2 = {
     day: 22,
   },
 };
-function reset(){
-  
-}
-beforeEach(() => {
-  console.log("beforEach")
-  todolist.value = [];
-});
+const testData3: Todo = {
+  code: 'testData3',
+  text: 'test3',
+  status: false,
+  time: 'time',
+  deadline: {
+    year: 2022,
+    month: 5,
+    day: 24,
+  },
+};
 
 describe('useTodos', () => {
+  beforeEach(() => {
+    todolist.value = [];
+  });
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
   it('空の配列に追加する', () => {
     // 追加テスト
-    add(testData);
-    expect(todolist.value).toEqual([testData]);
+    add(testData1);
+    expect(todolist.value).toEqual([testData1]);
   });
 
   it('空ではない配列にさらに追加する', () => {
-    add(testData);
+    add(testData1);
     add(testData2);
     expect(todolist.value).toEqual([
       {
@@ -71,7 +81,7 @@ describe('useTodos', () => {
   });
 
   it('配列から一つのTodo選んで編集する', () => {
-    todolist.value.push(testData);
+    todolist.value.push(testData1);
     todolist.value.push(testData2);
     edit({
       code: 'testData1',
@@ -111,13 +121,13 @@ describe('useTodos', () => {
   });
 
   it('配列からTodoを削除して空にする', () => {
-    todolist.value.push(testData);
+    todolist.value.push(testData1);
     remove('testData1');
     expect(todolist.value).toEqual([]);
   });
 
   it('配列からTodoを一つ削除する', () => {
-    todolist.value.push(testData);
+    todolist.value.push(testData1);
     todolist.value.push(testData2);
     remove('testData1');
     expect(todolist.value).toEqual([testData2]);
@@ -126,10 +136,8 @@ describe('useTodos', () => {
   it('未完のTodoを完了にする', () => {
     // システム時間を設定
     vi.setSystemTime(new Date('2022-06-03 16:09:19'));
-    todolist.value.push(testData);
-    console.log(testData)
+    todolist.value.push(testData1);
     checked('testData1');
-    console.log(testData)
     expect(todolist.value).toEqual([
       {
         code: 'testData1',
@@ -147,21 +155,52 @@ describe('useTodos', () => {
   });
 
   it('編集したデータを成型する', () => {
-    
     vi.setSystemTime(new Date('2022-06-03 16:09:19'));
-    const result = editData(testData, 'editTest!', '2022-06-01');
-    expect(result).toEqual(
-      {
-        code: 'testData1',
-        text: 'editTest!',
-        status: false,
-        time: '2022/6/3/16:9:19',
-        deadline: {
-          year: 2022,
-          month: 6,
-          day: 1,
-        },
-      }
-    );
+    const result = editData(testData1, 'editTest!', '2022-06-01');
+    expect(result).toEqual({
+      code: 'testData1',
+      text: 'editTest!',
+      status: false,
+      time: '2022/6/3/16:9:19',
+      deadline: {
+        year: 2022,
+        month: 6,
+        day: 1,
+      },
+    });
   });
+
+  it('新しいデータを成型する', () => {
+    vi.setSystemTime(new Date('2022-06-03 16:09:19'));
+    const spyRandom = vi //関数の結果を指定
+      .spyOn(Math, 'random')
+      .mockImplementation(() => 0.4904400539860695);
+    const result = createNewData('editTest!', '2022-06-01');
+
+    expect(result).toEqual({
+      code: 'fm6nle44on4',
+      text: 'editTest!',
+      status: false,
+      time: '2022/6/3/16:9:19',
+      deadline: {
+        year: 2022,
+        month: 6,
+        day: 1,
+      },
+    });
+  });
+
+  it('カレンダー用Todoリストを生成', () => {
+    todolist.value.push(testData1);
+    todolist.value.push(testData2);
+    todolist.value.push(testData3);
+    expect(calendarItems.value).toEqual({
+      '2022-05-22': [testData1, testData2],
+      '2022-05-24': [testData3],
+    });
+  });
+
+  // it("onMountでのデータのロード",()=>{
+  //   vi.spyOn(localStorage,"getItem").mockReturnValue('[{"code":"p1r8jdcs3gk","text":"b","status":false,"time":"2022/6/7/13:55:55","deadline":{"year":2022,"month":6,"day":13}}]')
+  // })
 });
--- src/Function/__tests__/utils.test.ts
+++ src/Function/__tests__/utils.test.ts
@@ -1,10 +1,43 @@
-import { getTime } from '../utils';
-import { test, expect, vi, it, describe, beforeEach } from 'vitest';
+import {
+  getTime,
+  generateID,
+  toObjectDeadline,
+  toStringDeadline,
+} from '../utils';
+import { expect, vi, it, describe, beforeEach, afterEach } from 'vitest';
+import { DeadLine } from '../../types';
 
 describe('utilities', () => {
+  beforeEach(() => {});
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
   it('時刻の取得', () => {
     vi.setSystemTime(new Date('2022-06-03 16:09:19'));
     expect(getTime()).toBe('2022/6/3/16:9:19');
     vi.useRealTimers();
+  });
+  it('ランダムなIDを生成', () => {
+    const spyRandom = vi
+      .spyOn(Math, 'random')
+      .mockImplementation(() => 0.4904400539860695);
+
+    expect(generateID()).toEqual('fm6nle44on4');
+  });
+  it('文字列の期限をオブジェクトに分解', () => {
+    const deadlineStr = '2022-06-07';
+    expect(toObjectDeadline(deadlineStr)).toEqual({
+      year: 2022,
+      month: 6,
+      day: 7,
+    });
+  });
+  it('オブジェクトの期限を文字列に結合', () => {
+    const deadlineObj: DeadLine = {
+      year: 2022,
+      month: 6,
+      day: 7,
+    };
+    expect(toStringDeadline(deadlineObj)).toEqual('2022-06-07');
   });
 });
