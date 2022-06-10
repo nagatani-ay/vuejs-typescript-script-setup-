f261dbd6634957931f53c5e550c978e3b5991a05
nagatani-ay <nagatani@ay-sys.com>
2022-06-10 17:02:13 +0900

    1 file modified

--- src/components/Schedule/__tests__/ScheduleItem.test.ts
+++ src/components/Schedule/__tests__/ScheduleItem.test.ts
@@ -41,7 +41,13 @@ describe('ScheduleItem', () => {
   it('v-ifの表示切替', async () => {
     const wrapper = mount(ParentComponent);
     const array = wrapper.findAll('div');
-    expect(array[0].html()).toEqual('<div class="todo__item"><p>test1</p><!--v-if--></div>');
+    console.log(array[0].html());
+    expect(array[0].html()).toContain(
+      `<div class="todo__item">
+        <p>test1</p>
+        <!--v-if-->
+      </div>`
+    );
     await array[0].trigger('dblclick');
     // expect(array[0].html()).toContain('');
   });
